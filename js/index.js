56d04cafad201f4d545b7ee674952c5972a1916c
Alejandro Ahijado Ibarra <alejandro.ahijado@gmail.com>
2023-08-10 14:38:44 +0200

    DOM elements added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
-console.log("Hello world")
+class Player {
+    constructor () {
+        this.positionX = 50;
+        this.positionY = 0;
+        this.width = 10;
+        this.height = 10;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement() {
+        //create DOM element
+        this.domElement = document.createElement("div");
+        //set id
+        this.domElement.id = "player";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+        //append to the DOM
+        const parentElement = document.getElementById("board");
+        parentElement.appendChild(this.domElement);
+    }
+    moveLeft(){
+        this.positionX--;
+        this.domElement.style.left = this.positionX + "vw";
+    }
+    moveRight(){
+        this.positionX++;
+        this.domElement.style.left = this.positionX + "vw";
+    }
+}
+
+const player = new Player();
+
+document.addEventListener("keydown", (event) => {
+    if (event.code === "ArrowLeft") {
+        return player.moveLeft()
+    } else if (event.code === "ArrowRight") {
+        return player.moveRight()
+    }
+});
